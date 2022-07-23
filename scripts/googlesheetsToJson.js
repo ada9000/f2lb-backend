0a529026d7b14ab68cfbe985a721e7c912b989f8
jack <jack.7e@protonmail.com>
2022-07-23 20:41:58 +0100

    covert gsheet to meta w/ epochs

--- scripts/googlesheetsToJson.js
+++ scripts/googlesheetsToJson.js
@@ -2,24 +2,27 @@
 const axios = require('axios');
 const dotenv = require("dotenv")
 const blockfrost = require('@blockfrost/blockfrost-js');
+const {getEpoch} = require('../api/blockfrost')
 dotenv.config()
 const GOOGLE_API = process.env.GOOGLE_API
 const {bech32} = require('bech32');
 
 
 async function getAllPools(offset=0, data=[]){
-    return await axios(`https://api.koios.rest/api/v0/pool_list?offset=${offset}&limit=500`)
+    return await axios(`https://api.koios.rest/api/v0/pool_list?offset=${offset}&limit=900`)
     .then(res => {
         if (res.data.length > 0){
             data.push(...res.data);
             console.log(`${offset} with len ${res.data.length} last ${JSON.stringify(data[data.length-1])}`);
-            return getAllPools(offset + 500, data);
+            return getAllPools(offset + 900, data);
         }
         return data
     }).catch(e => {console.log(e)})
 }
 
 async function getGooglesheetData(){
+    const currentEpoch = await getEpoch()
+
     const gSheetData = await axios(`https://sheets.googleapis.com/v4/spreadsheets/1-mA8vY0ZtzlVdH4XA5-J4nIZo4qFR_vFbnBFkpMLlYo/values/MainQueue?key=${GOOGLE_API}`)
     .then(res => {
         //const header = res.data.values[1];
@@ -29,15 +32,8 @@ async function getGooglesheetData(){
     })
 
     const poolList = await getAllPools().then(d => {return d});
-    for (x in poolList){
-        //console.log(poolList[x].ticker)
-        if (poolList[x].poolId === "pool1xpfe5q3v3axrjdc8h38taaa93frq3m9pfewxk46x4r6jgy2yj5n")
-        {
-            console.log("found it")
-            console.log(poolList[x].ticker)
-        }
-    }
 
+    var targetEpoch = parseInt(currentEpoch.epoch);
     for(row in gSheetData){
         const stakeHex = gSheetData[row][8];
         const stakeAddressDecoded = 'e1' + stakeHex;
@@ -50,21 +46,73 @@ async function getGooglesheetData(){
             );
         }
         const ticker = gSheetData[row][2];
-        const poolData = poolList.find(p => p.ticker === ticker.toUpperCase());
+        const epochsGranted = parseInt(gSheetData[row][4]);
+        
+        var epochs = []
+        for (let i = 0; i < epochsGranted; i++){
+            epochs.push(parseInt(targetEpoch + i))
+        }
+        targetEpoch += epochsGranted;
+
+
+        const poolData = await poolList.find(p => p.ticker !== null && p.ticker.toUpperCase() === ticker.toUpperCase());
         // handle the maple edge case
+
+
         var poolId = undefined;
         if(poolData !== undefined){
             poolId = poolData.pool_id_bech32
         }
-        else if(ticker === "MAPLE"){
+        else if(ticker.toUpperCase() === "MAPLE"){
             // handle MAPLE edge case
+            console.log(`🐛 ${ticker}`)
             poolId = "pool1xpfe5q3v3axrjdc8h38taaa93frq3m9pfewxk46x4r6jgy2yj5n"
         }
-        else if(ticker === "ASTCH"){
+        else if(ticker.toUpperCase() === "ASTCH"){
             // handle ASTCH edge case
+            console.log(`🐛 ${ticker}`)
             poolID = "pool1q2fh3cl6rx0wv5gry4qx5l4h65qpjf7x99xmq66nqj2fj5g6u9z"
         }
-        console.log(`ticker=${ticker} : stake=${bech32StakeAddress} : poolId=${poolId}`)
+        else {
+            throw `Pool not found with ${ticker} when using https://api.koios.rest/api/v0/pool_list`;
+        }
+
+        const poolMeta = await axios.post(`https://api.koios.rest/api/v0/pool_metadata`,{
+            "_pool_bech32_ids":[
+                poolId
+            ]
+        }).then(res => {return  res.data })
+
+        //console.log(`\n\n${ticker}\nticker=${ticker} : stake=${bech32StakeAddress} : poolId=${poolId}`)
+        //console.log(poolMeta[0])
+
+        const website = poolMeta[0].meta_json ? poolMeta[0].meta_json.homepage : null;
+        const description = poolMeta[0].meta_json ? poolMeta[0].meta_json.description : null;
+        const metaUrl = poolMeta[0].meta_url;
+        
+        var meta = null;
+        try{
+            meta = await axios(metaUrl).then(res => {return res.data});
+        } catch(e){};
+        var extendedMeta = null
+        try{
+            extendedMeta = await axios(meta.extended).then(res => {return res.data});
+        }catch(e){};
+
+
+
+        const image = extendedMeta ? extendedMeta.info.url_png_logo : null;
+        const pool = {
+            poolIdBech32:poolId,
+            ticker:ticker,
+            website:website,
+            imageUrl:image,
+            description:description,
+            epochs:epochs,
+        }
+
+        console.log(pool)
+
     }
 }
 
