1e7be51f9e4b720dc7c3dba8f11e7d80739ccf8e
spklpool <spklpool@gmail.com>
2022-07-24 14:37:00 -0400

    making naming consistent with website repo

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker stop f2lb
-docker rm f2lb
-docker build . -t node-f2lb
-docker run --name f2lb -p 4000:4000 -d node-f2lb
+docker stop f2lb-backend
+docker rm f2lb-backend
+docker build . -t f2lb-backend-image
+docker run --name f2lb-backend -p 4000:4000 -d f2lb-backend-image
