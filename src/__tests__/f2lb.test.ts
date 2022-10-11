be12ec1b1253c501562c2339fe9c4081f3a7f540
jack <jack.7e@protonmail.com>
2022-10-11 22:14:02 +0100

    updated testing suite

--- src/__tests__/f2lb.test.ts
+++ src/__tests__/f2lb.test.ts
@@ -1,1 +1,9 @@
 // TODO: implement tests
+
+describe("Recalculate Queue", () => {
+  it("no change", () => {
+    const poolMock = require("./__mocks__/poolsMock.json");
+    console.log(poolMock);
+    expect(true).toBeTruthy();
+  });
+});
